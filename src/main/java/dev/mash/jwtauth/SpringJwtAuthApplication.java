39790ed7f5e0be3940f60116de00ee4d42bade36
Mikhail Shamanov <shamanovma@gmail.com>
2024-06-01 20:22:57 +0300

    Created initial project

---
+++ src/main/java/dev/mash/jwtauth/SpringJwtAuthApplication.java
@@ -0,0 +1,12 @@
+package dev.mash.jwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwtAuthApplication.class, args);
+    }
+}
