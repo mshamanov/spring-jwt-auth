3efbf334698160f0d7fb67f9b4337dea47ff5ec8
Mikhail Shamanov <shamanovma@gmail.com>
2024-06-03 22:57:51 +0300

    Removed unnecessary throws, removed redundant imports

--- src/main/java/dev/mash/jwtauth/exception/JwtAuthenticationEntryPoint.java
+++ src/main/java/dev/mash/jwtauth/exception/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,5 @@
 package dev.mash.jwtauth.exception;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,8 +7,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-
-import java.io.IOException;
 
 /**
  * Handles any {@code AccessDeniedException} and {@code AuthenticationException} thrown within the filter chain.
@@ -27,7 +24,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
+                         AuthenticationException authException) {
         this.resolver.resolveException(request, response, null, authException);
     }
 }
