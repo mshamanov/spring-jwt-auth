66aa0a8de9eb17852c7de62d56c9ba0308fc61cd
Mikhail Shamanov <shamanovma@gmail.com>
2024-06-04 22:58:59 +0300

    Added initial sql data to be loaded on application startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,45 @@
+BEGIN;
+
 INSERT INTO roles (type)
-SELECT 'ROLE_USER' WHERE NOT EXISTS (SELECT * FROM roles WHERE type = 'ROLE_USER');
+VALUES ('ROLE_USER') ON CONFLICT DO NOTHING;
 INSERT INTO roles (type)
-SELECT 'ROLE_ADMIN' WHERE NOT EXISTS (SELECT * FROM roles WHERE type = 'ROLE_ADMIN');
+VALUES ('ROLE_ADMIN') ON CONFLICT DO NOTHING;
+
+INSERT INTO users (username, password, email)
+VALUES ('user', '$2a$10$GNdiqyfyUtSfanS24/SlKOtmpil6CChLEUm6EANC/7leIPI7Fg0/i',
+        'user@example.com') ON CONFLICT DO NOTHING;
+INSERT INTO users (username, password, email)
+VALUES ('admin', '$2a$10$GNdiqyfyUtSfanS24/SlKOtmpil6CChLEUm6EANC/7leIPI7Fg0/i',
+        'admin@example.com') ON CONFLICT DO NOTHING;
+
+INSERT INTO user_role (user_id, role_id)
+SELECT u.id, r.id
+FROM users u
+         JOIN roles r ON u.username = 'user'
+    AND r.type = 'ROLE_USER'
+WHERE NOT EXISTS (SELECT *
+                  FROM user_role ur
+                  WHERE ur.user_id = u.id
+                    AND ur.role_id = r.id);
+
+INSERT INTO user_role (user_id, role_id)
+SELECT u.id, r.id
+FROM users u
+         JOIN roles r ON u.username = 'admin'
+    AND r.type = 'ROLE_USER'
+WHERE NOT EXISTS (SELECT *
+                  FROM user_role ur
+                  WHERE ur.user_id = u.id
+                    AND ur.role_id = r.id);
+
+INSERT INTO user_role (user_id, role_id)
+SELECT u.id, r.id
+FROM users u
+         JOIN roles r ON u.username = 'admin'
+    AND r.type = 'ROLE_ADMIN'
+WHERE NOT EXISTS (SELECT *
+                  FROM user_role ur
+                  WHERE ur.user_id = u.id
+                    AND ur.role_id = r.id);
+
+COMMIT;
